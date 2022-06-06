a0f5a2f3e421ae3c856b9f567b8d160143c3c106
湃 <2805795867@qq.com>
2022-06-06 09:42:46 +0800

    master分支在第九行添加新内容

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("新建文件");
         System.out.println("第一次更新");
         System.out.println("第二次更新");
-        System.out.println("dev添加分支");
+        System.out.println("master添加分支");
         System.out.println("master分支在第九行添加新内容");
     }
 }
